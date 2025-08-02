9588f0adc8d03b5f03756dadfca2d816c94e8207
Tal <taltal2345@gmail.com>
2025-08-02 19:27:34 +0300

    fix: restore original background color and effects

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,7 +2,7 @@ import MainHeadline from "../components/MainHeadline";
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-[#fdfcf9]">
+    <div className="min-h-screen">
       <MainHeadline />
     </div>
   );
